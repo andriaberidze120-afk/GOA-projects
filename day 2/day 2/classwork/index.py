11513958ec2032a4ba187852c381e1f1c88e344c
andriaberidze120-afk <andriaberidze120@gmail.com>
2025-10-05 22:37:08 +0400

    third commit

---
+++ day 2/day 2/classwork/index.py
@@ -0,0 +1,13 @@
+print("qartvelobit vamayob")
+print(31)
+print(3.14)
+
+name = "Andria"
+age = 13
+food = "xili"
+colour = "tetri"
+
+print(name)
+print(age)
+print(food)
+print(colour)
---
+++ day 2/day 2/homework/index.py
@@ -0,0 +1,39 @@
+#string --> ცვლადის ტიპი, რომელშიც ინახება ტექსტური ინფორმაცია
+#integer --> ცვლადის ტიპი, რომელშიც მთელი რიცხვები ინახება
+#float --> ცვლადის ტიპი, რომელშიც ინახება ათწილადი რიცხვები
+
+#ცვლადი --> არის ერთგვარი ყუთი, რომელშიც ინახება გარკვეული ტიპის ინფორმაცია, რომელიც თუ დაგვჭირდება შეგვიძლია გამოვიძახოთ
+
+print("me mqvia Andria")
+print("chemi gvaria beridze")
+print("miyvars xili")
+print("chemi sayvareli feria tetri")
+print("vamayob qartvelobita da qristianobit")
+
+print(31)
+print(2012)
+print(1)
+print(3)
+print(25)
+
+print(3.14)
+print(1.61)
+print(2.71)
+print(186.282)
+print(3.5)
+
+place = "Georgia"
+brand = "Nike"
+food = "Cucumber"
+drink = "Water"
+entertainment = "Book"
+
+print("Best country is:", place)
+print("Best brand is:", brand)
+print("Best food country is:", food)
+print("Best drink is:", drink)
+print("Best entertainment is:", entertainment)
+
+
+
+
