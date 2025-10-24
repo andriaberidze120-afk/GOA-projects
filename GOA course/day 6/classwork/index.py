19e571620e3179cac45f38fe6444ca038ebb7b3a
andriaberidze120-afk <andriaberidze120@gmail.com>
2025-10-24 22:26:43 +0400

    sixth commit

---
+++ GOA course/day 6/classwork/index.py
@@ -0,0 +1,28 @@
+#input-ს ყოველთვის შემოაქვს string, ანუ ტექსტური ტიპის ინფორმაცია, იმ შემთხვევაში თუ მას არ აქვს სპეციალური ნიშანი მიწერილი
+
+my_age = 13
+print(my_age)
+num_1 = 3.14
+num_2 = 1.62
+print(num_2 + num_1)
+print(num_2 - num_1)
+print(num_2 * num_1)
+print(num_2 / num_1)
+
+favourite_colour = input("Enter your favourite colour:")
+favourite_animal= input("Enter your favourite animal:")
+favourite_place = input("Enter your favourite place:")
+print("Your favourite colour is:",favourite_colour, "Your favourite animal is:","Your favourite place is:", favourite_place)
+
+car1 = input("Enter car brand:")
+car2 = input("Enter car brand:")
+car3 = input("Enter car brand:")
+car4 = input("Enter car brand:")
+
+print(car1)
+print(car2)
+print(car3)
+print(car4)
+
+
+
---
+++ GOA course/day 6/homework/index.py
@@ -0,0 +1,15 @@
+num_1 = int(input("Enter your first number:"))
+num_2 = int(input("Enter your second number:"))
+num_3 = int(input("Enter your third number:"))
+print(num_3 / 3)
+print(num_1 + num_2)
+#data type - არის მონაცემთა ტიპი, რომლის მიხედვითაც განვსაზღვრავთ, თუ როგორი ტიპის მონაცემი შემოდის, მაგ:ტექსტი, რიცხვი, ათწილადი და ა.შ.
+#კონკრეტულად პითონში ჩვენ (ჯერჯერობით) ვიცით 3 ტიპის მონაცემი: string-რითაც ავღნიშნავთ ტექსტს, integer-რითაც აღინიშნება მთელი რიცხვები და float-რომლითაც ავღნიშნავთ ათწილად რიცხვებს
+mom = input("mom:")
+dad = input("dad:")
+sis_1 = input("first sister:")
+sis_2 = input("second sister:")
+bro = input("brother:")
+me = input("me:")
+print("Hi!, my name is:",me," ","my mom is called:",mom,".","my dad is called:",dad,".","I have twin sisters too! First sister's name is:",sis_1,".","Second sister's name is",sis_2,".","My bother's name is:",bro,".")
+
